719825d6dee33779b1a4499ae1df226a143723cb
Priya v <priyavel7358@gmail.com>
2023-06-22 19:02:42 +0530

    task1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { MultiSelect } from "react-multi-select-component";
 
-function App() {
+const options = [
+  { label: "Pasta & risotto", value: "Pasta & risotto." },
+  { label: "Salad", value: "Salad" },
+  { label: "Bread & doughs.", value: "Bread & doughs." },
+  { label: "Curry", value: "Curry" },
+  { label: "Vegetable sides.", value: "Vegetable sides." },
+  { label: "Cereals", value: "Cereals" },
+  { label: "Soup", value: "Soup" },
+  { label: "Antipasti", value: "Antipasti" },
+  { label: "Roast", value: "Roast" },
+  { label: "Baked goods.", value: "Baked goods" },
+  { label: " Coconut rice.", value: " Coconut rice." },
+  { label: "Pepper rice.", value: "Pepper rice." },
+  { label: " Lentil rice.", value: " Lentil rice." },
+  { label: "Curry leaves rice.", value: "Curry leaves rice." },
+  { label: "Ghee rice.", value: "Ghee rice." },
+  { label: "Mutton biriyani", value: "Mutton biriyani" },
+  { label: "Chicken Biriyani", value: "Chicken Biriyani" },
+  { label: "Prawn biriyani", value: "Prawn biriyani" },
+  { label: "beaf biriyani", value: "beaf biriyani" },
+  { label: "Fish biriyani", value: "Fish biriyani" },
+  { label: "Egg biriyani", value: "Egg biriyani" },
+  { label: "Chicken 65", value: "Chicken 65" },
+  { label: "Mango ", value: "mango" },
+  { label: "Strawberry ", value: "strawberry" },
+  { label: "Grapes ", value: "grapes" },
+  { label: "banana ", value: "banana" },
+  { label: "fries ", value: "fries" },
+  { label: "peri peri fries ", value: "peri peri fries" },
+  { label: "chessy balls ", value: "chessy balls" },
+  { label: "pizza ", value: "pizza" },
+  { label: "burger ", value: "burger" },
+  { label: "egg masala ", value: "egg masala" },
+  { label: "fish fries ", value: "fish fries" },
+];
+
+const App = () => {
+  const [selected, setSelected] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: "20px" }}>
+      <h1>Select your favourite menu</h1>
+      <div
+        style={{
+          margin: "50px",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <div style={{ width: "90%", margin: "10px" }}>
+          <MultiSelect
+            options={options.sort()}
+            value={selected}
+            onChange={setSelected}
+            labelledBy="Select"
+          />
+        </div>
+        <div style={{ width: "20%", margin: "10px" }}>
+          <button
+            style={{
+              height: "100%",
+              color: "white",
+              backgroundColor: "blue",
+              border: "none",
+              borderRadius: "5px",
+            }}
+            onClick={() => (window.location = "mailto:priyavel7358@gmail.com")}
+          >
+            Send mail
+          </button>
+        </div>
+      </div>
+      <div></div>
     </div>
   );
-}
+};
 
 export default App;
